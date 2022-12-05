a938a7a8fc6591b1612dfd261652873bd95ecea1
afshintrg <ataraghijoo@gmial.com>
2022-12-05 17:14:22 +0330

    Add 'Add news' view

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeNews.as_view()),
+    path('', views.AddNewsView.as_view()),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,21 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import NewsSerializer
+import requests
+from rest_framework import status
 
 
-class HomeNews(APIView):
+class AddNewsView(APIView):
+
     def get(self, request):
-        return Response('hello')
+        resp = requests.get('https://feeds.npr.org/1004/feed.json')
+        items = resp.json()['items'][0:5]
+        for item in items:
+            item['author'] = item['author']['name'] if 'author' in item else None
+            item['tags'] = '-'.join(item['tags']) if 'tags' in item else None
+            item['attachments'] = str(item['attachments']) if 'attachments' in item else None
+        srz_data = NewsSerializer(data=items, many=True, partial=True)
+        if srz_data.is_valid():
+            srz_data.create(srz_data.validated_data)
+            return Response('items added', status=status.HTTP_201_CREATED)
+        return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
